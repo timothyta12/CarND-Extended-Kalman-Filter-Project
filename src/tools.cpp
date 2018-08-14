afcab417116a02ec38f9da0d1e530ab1796e65a5
timothyta12 <timothyta12@gmail.com>
2018-08-14 05:38:41 +0000

    Added rmse error calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,11 +15,36 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-}
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  
+  // Checking validity of input
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+  	return rmse;
+  }
+      
+  // Accumulate
+  for (int i=0; i<estimations.size(); i++) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
+  
+  // Calculate the mean
+  rmse = rmse / estimations.size();
 
+  // Calculate square root
+  rmse = rmse.array().sqrt();
+      
+  return rmse;
+}
+  
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
   TODO:
     * Calculate a Jacobian here.
   */
+  
+  // Check for division by zero
+  
 }
